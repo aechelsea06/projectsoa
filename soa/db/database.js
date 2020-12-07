b0a269094c649bdf519669de4090d6364f475cf4
twilight6022 <60873819+twilight6022@users.noreply.github.com>
2020-12-07 19:33:58 +0700

    new

--- soa/db/database.js
+++ soa/db/database.js
@@ -3,8 +3,8 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
-    password: 'pluem',
-    port: 5432,
+    password: '123456',
+    port: 5433,
 })
 
 const getAllCountry = async() => {
