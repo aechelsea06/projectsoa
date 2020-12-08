ad8e61eb065bf75b45fd558eef74019fff2e821b
daruma025 <71595066+daruma025@users.noreply.github.com>
2020-12-08 21:31:01 +0700

    update

--- soa/db/database.js
+++ soa/db/database.js
@@ -3,8 +3,8 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
-    password: '123456',
-    port: 5433,
+    password: 'pluem',
+    port: 5432,
 })
 
 async function getdata(){
