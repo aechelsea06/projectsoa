67ae34980c7db8d880e052f6e64247116efdf6b4
aechelsea06 <64302473+aechelsea06@users.noreply.github.com>
2020-12-08 18:02:40 +0700

    up

--- soa/routes/index.js
+++ soa/routes/index.js
@@ -2,99 +2,34 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/database');
 
-router.get('/', async function(req, res, next) {
-    const result = await db.getAllCountry();
-    const confirmed = await db.getAllConfirmed();
-    const recovered = await db.getAllRecovered();
-    const death = await db.getAllDeath();
+router.get('/global',async function(req, res, next) {
+    const result = await db.getdata();
+    console.log(result.rows);
+    res.render('global', { global: result.rows });
+  });
 
-    let objectCountry = [];
-    for (const key in result.rows) {
-        objectCountry[key] = {
-            state: result.rows[key].state,
-            country: result.rows[key].country,
-            confirmed: confirmed.rows[key].confirmed,
-            recovered: recovered.rows[key].confirmed,
-            death: death.rows[key].confirmed
-        }
-    }
-    res.render('index', { bring: objectCountry });
-});
+  router.get('/',async function(req, res, next) {
+    const result = await db.getdata();
+    console.log(result.rows);
+    res.render('index', { index: result.rows });
+  });
 
-router.get('/map', async function(req, res, next) {
-    const totalConfirmed = await db.getTotalConfirm();
-    const totalRecovered = await db.getTotalRecovered();
-    const totalDeath = await db.getTotalDeath();
-    const getLatLong = await db.getLatLong();
+  router.get('/thailand',async function(req, res, next) {
+    const result = await db.getth();
+    console.log(result.rows);
+    res.render('thailand', { thailand: result.rows });
+  });
 
-    const objectTotal = {
-        totalConfirmed: totalConfirmed.rows[0].confirmed,
-        totalRecovered: totalRecovered.rows[0].recovered,
-        totalDeath: totalDeath.rows[0].death
-    }
-    res.render('map', { totalObject: objectTotal, Maps: getLatLong.rows });
-});
+  router.get('/protect',async function(req, res, next) {
+    const result = await db.getdata();
+    console.log(result.rows);
+    res.render('protect', { protect: result.rows });
+  });
 
-router.get('/protect', async function(req, res, next) {
-    res.render('protect');
-});
-
-router.get('/global', async function(req, res, next) {
-    const result = await db.getAllCountry();
-    const confirmed = await db.getAllConfirmed();
-    const recovered = await db.getAllRecovered();
-    const death = await db.getAllDeath();
-
-    let objectCountry = [];
-    for (const key in result.rows) {
-        objectCountry[key] = {
-            state: result.rows[key].state,
-            country: result.rows[key].country,
-            confirmed: confirmed.rows[key].confirmed,
-            recovered: recovered.rows[key].confirmed,
-            death: death.rows[key].confirmed
-        }
-    }
-    res.render('global', { bring: objectCountry });
-});
-
-router.get('/thailand', async function(req, res, next) {
-    const resultTH = await db.getAllCountryTH();
-    const confirmedTH = await db.getAllConfirmedTH();
-    const recoveredTH = await db.getAllRecoveredTH();
-    const deathTH = await db.getAllDeathTH();
-
-    let objectCountry = [];
-    for (const key in resultTH.rows) {
-        objectCountry[key] = {
-            state: resultTH.rows[key].state,
-            country: resultTH.rows[key].country,
-            confirmed: confirmedTH.rows[key].confirmed,
-            recovered: recoveredTH.rows[key].confirmed,
-            death: deathTH.rows[key].confirmed
-        }
-    }
-    res.render('thailand', { countrys: objectCountry });
-});
-
-router.get('/totalcases', async function(req, res, next) {
-    const resultTH = await db.getAllCountryTH();
-    const confirmedTH = await db.getAllConfirmedTH();
-    const recoveredTH = await db.getAllRecoveredTH();
-    const deathTH = await db.getAllDeathTH();
-
-    let objectCountry = [];
-    for (const key in resultTH.rows) {
-        objectCountry[key] = {
-            state: resultTH.rows[key].state,
-            country: resultTH.rows[key].country,
-            confirmed: confirmedTH.rows[key].confirmed,
-            recovered: recoveredTH.rows[key].confirmed,
-            death: deathTH.rows[key].confirmed
-        }
-    }
-    res.render('totalcases', { total: objectCountry });
-});
-
+  router.get('/map',async function(req, res, next) {
+    const result = await db.getmap();
+    console.log(result.rows);
+    res.render('map', { maps: result.rows });
+  });
 
 module.exports = router;
