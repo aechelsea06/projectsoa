d7426e525d71e510950a59048663b20328d20e35
twilight6022 <60873819+twilight6022@users.noreply.github.com>
2020-12-08 19:28:49 +0700

    soa

--- soa/db/database.js
+++ soa/db/database.js
@@ -3,8 +3,8 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
-    password: '555678',
-    port: 5432,
+    password: '123456',
+    port: 5433,
 })
 
 async function getdata(){
@@ -38,11 +38,34 @@ async function getmap(){
     return data;
 
 }
+async function gettotals(){
+    const sql = `select   sum(confirmed_csv."3/23/2020") as confirm3 ,  sum(death_csv."3/23/2020") as dead3 ,  sum(recovered_csv."3/23/2020") as recover3,
+    sum(confirmed_csv."2/23/2020") as confirm2 ,  sum(death_csv."2/23/2020") as dead2 ,  sum(recovered_csv."2/23/2020") as recover2,
+    sum(confirmed_csv."1/23/2020") as confirm1 ,  sum(death_csv."1/23/2020") as dead1 ,  sum(recovered_csv."3/23/2020") as recover1
+        from city_csv,confirmed_csv,death_csv,recovered_csv
+        where city_csv.num = confirmed_csv.num and city_csv.num = death_csv.num and city_csv.num = recovered_csv.num and  (confirmed_csv."3/23/2020" != 0 or  death_csv."3/23/2020" !=0 or recovered_csv."3/23/2020"!=0) `
+    const data = await pool.query(sql);
+    console.log(data);
+    return data;
+
+}
+async function getsum(){
+    const sql = `select  sum(confirmed_csv."3/23/2020") as confirm , sum(death_csv."3/23/2020") as death , sum(recovered_csv."3/23/2020") as recovered 
+    from city_csv,confirmed_csv,death_csv,recovered_csv
+    where city_csv.num = confirmed_csv.num and city_csv.num = death_csv.num and city_csv.num = recovered_csv.num `
+    const data = await pool.query(sql);
+    // console.log(data);
+    return data;
+
+}
+
 
 
 module.exports = {
     getdata,
     getth,
+    getsum,
+    gettotals,
     getmap
 
 }
